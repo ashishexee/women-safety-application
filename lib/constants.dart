2b52e7ca841041b37b72e892e27bf1a71da3519b
ashish <144021866+ashishexee@users.noreply.github.com>
2025-04-07 17:51:22 +0530

    Delete constants.dart

--- lib/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const themecolor = Color(0xFFfc4572);
-void goto(BuildContext context, Widget nextscreen) {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => nextscreen));
-}
