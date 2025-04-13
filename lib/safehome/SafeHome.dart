6ec6f5499b5f7e4861bc4d535bb40dc522dca55e
ashish <144021866+ashishexee@users.noreply.github.com>
2025-04-13 19:16:19 +0530

    Update SafeHome.dart

--- lib/safehome/SafeHome.dart
+++ lib/safehome/SafeHome.dart
@@ -1,7 +1,97 @@
+// ignore: file_names
+import 'package:background_sms/background_sms.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:woman_safety_app/components/primary_button.dart';
+import 'package:woman_safety_app/constants/constants.dart';
+import 'package:woman_safety_app/db/db_helper.dart';
+import 'package:woman_safety_app/models/contacts.dart';
 
-class Safehome extends StatelessWidget {
+class Safehome extends StatefulWidget {
   const Safehome({super.key});
+
+  @override
+  State<Safehome> createState() => _SafehomeState();
+}
+
+class _SafehomeState extends State<Safehome> {
+  Position? _currentPosition;
+  String? _currentAddress;
+  LocationPermission? permission;
+  _getpermission() async => await [Permission.sms].request();
+  _ispermissiongranded() async {
+    return await Permission.sms.status.isGranted;
+  }
+
+  _sendSms(String phonenumber, String message, {int? simslot}) async {
+    await BackgroundSms.sendMessage(
+      phoneNumber: phonenumber,
+      message: message,
+      simSlot: simslot,
+    ).then((SmsStatus status) {
+      if (status == SmsStatus.sent) {
+        showFlutterToast('Message has been send');
+      } else {
+        showFlutterToast('Could not send Message');
+      }
+    });
+  }
+
+  _getcurrentlocation() async {
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    if (permission == LocationPermission.deniedForever) {
+      showFlutterToast(
+        "Location permission is permanently denied. Please check your device settings.",
+      );
+    }
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+        forceAndroidLocationManager: true,
+      );
+      if (mounted) {
+        setState(() {
+          _currentPosition = position;
+        });
+      }
+      _getaddresslocation();
+    } catch (e) {
+      showFlutterToast(e.toString());
+    }
+  }
+
+  _getaddresslocation() async {
+    try {
+      if (_currentPosition == null) {
+        showFlutterToast("Current position is not available.");
+        return;
+      }
+      List<Placemark> placemark = await placemarkFromCoordinates(
+        _currentPosition!.latitude,
+        _currentPosition!.longitude,
+      );
+      Placemark place = placemark[0];
+      if (mounted) {
+        setState(() {
+          _currentAddress =
+              "${place.locality},${place.postalCode},${place.street},${place.administrativeArea},${place.subAdministrativeArea}";
+        });
+      }
+    } catch (e) {
+      showFlutterToast(e.toString());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getcurrentlocation();
+  }
 
   void showModelissafe(BuildContext context) {
     showModalBottomSheet(
@@ -11,26 +101,115 @@ class Safehome extends StatelessWidget {
       ),
       builder: (context) {
         return Container(
-          height: 400,
+          height: 550,
           padding: EdgeInsets.all(16),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text(
-                'Location Sharing',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+                child: CircleAvatar(
+                  radius: 40,
+                  backgroundColor: const Color.fromARGB(255, 255, 230, 236),
+                  child: Icon(Icons.location_on, size: 50, color: themecolor),
+                ),
               ),
               SizedBox(height: 20),
-              Text(
-                'You can share your location with trusted contacts for safety.',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16),
+              AnimatedOpacity(
+                duration: Duration(milliseconds: 300),
+                opacity: 1.0,
+                curve: Curves.easeInOut,
+                child: Text(
+                  'Location Sharing',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
               ),
-              Spacer(),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
+              SizedBox(height: 20),
+              AnimatedOpacity(
+                duration: Duration(milliseconds: 300),
+                opacity: 1.0,
+                curve: Curves.easeInOut,
+                child: Text(
+                  'You can share your location with trusted contacts for safety',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: Colors.black54),
+                ),
+              ),
+              ValueListenableBuilder(
+                valueListenable: ValueNotifier(_currentAddress),
+                builder: (context, value, child) {
+                  return Text(value ?? '');
                 },
-                child: Text('Close'),
+              ),
+              SizedBox(height: 10),
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+                child: PrimaryButton(
+                  title: 'GET LOCATION',
+                  onPressed: () {
+                    _getcurrentlocation();
+                    _getaddresslocation();
+                  },
+                ),
+              ),
+              SizedBox(height: 15),
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+                child: PrimaryButton(
+                  title: 'SEND ALERT',
+                  onPressed: () async {
+                    List<TContact> contactList =
+                        await DbHelper().getContactList();
+                    // we basically want to make the recp like this
+                    // 7678250729;8178424572;981061858;9873853802
+                    String recp = "";
+                    int index = 1;
+                    for (TContact contact in contactList) {
+                      recp += contact.number;
+                      if (index != contactList.length) {
+                        recp += ";";
+                        index++;
+                      }
+                    }
+                    String message =
+                        "https://www.google.com/maps/search/?api=1&query=${_currentPosition!.latitude}%2C${_currentPosition!.longitude}. $_currentAddress";
+                    if (await _ispermissiongranded()) {
+                      for (var element in contactList) {
+                        _sendSms(
+                          element.number,
+                          "please reach out to me at this address I am in trouble $message",
+                          simslot: 1,
+                        );
+                      }
+                      showFlutterToast('Sending Message.......');
+                    } else {
+                      showFlutterToast('Failed to send SMS');
+                    }
+                  },
+                ),
+              ),
+              SizedBox(height: 30),
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+                child: InkWell(
+                  child: CircleAvatar(
+                    radius: 40,
+                    backgroundColor: const Color.fromARGB(255, 255, 230, 236),
+                    child: Icon(Icons.close, size: 50, color: themecolor),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
               ),
             ],
           ),
