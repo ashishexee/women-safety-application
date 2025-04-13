cceff334960dd132ef6e66422d78376d20b2819e
ashish <144021866+ashishexee@users.noreply.github.com>
2025-04-13 19:15:38 +0530

    Update home_screen.dart

--- lib/child/bottom_pages.dart/home_screen.dart
+++ lib/child/bottom_pages.dart/home_screen.dart
@@ -14,20 +14,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  // const HomeScreen({super.key});
   int qindex = 0;
 
-  // initially the qoute index is 0
   void getrandom() {
     setState(() {
       Random random = Random();
-      qindex = random.nextInt(6); // Assuming there are 6 quotes
+      qindex = random.nextInt(6);
     });
   }
 
   @override
   void initState() {
-    // init state is basically the first function that is called back by the user
     getrandom();
     super.initState();
   }
@@ -36,61 +33,60 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CustomAppbar(index: qindex, ontap: getrandom), // custom app bar
-              CustomCarouel(onSlideChange: getrandom),
-              Center(
-                child: ShaderMask(
-                  shaderCallback:
-                      (bounds) => LinearGradient(
-                        colors: [Colors.red, Colors.orange],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                      ).createShader(bounds),
-                  child: Text(
-                    "Emergencies",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color:
-                          Colors
-                              .white, // This color is overridden by the gradient
-                    ),
-                  ),
-                ),
-              ),
-              Emergency(),
-              Center(
-                child: ShaderMask(
-                  shaderCallback:
-                      (bounds) => LinearGradient(
-                        colors: [Colors.red, Colors.orange],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                      ).createShader(bounds),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          // Add SingleChildScrollView here
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomAppbar(index: qindex, ontap: getrandom),
+                CustomCarouel(onSlideChange: getrandom),
+                Center(
+                  child: ShaderMask(
+                    shaderCallback:
+                        (bounds) => LinearGradient(
+                          colors: [Colors.red, Colors.orange],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ).createShader(bounds),
                     child: Text(
-                      "Explore Livesafe",
+                      "Emergencies",
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
-                        color:
-                            Colors
-                                .white, // This color is overridden by the gradient
+                        color: Colors.white,
                       ),
                     ),
                   ),
                 ),
-              ),
-              Livesafe(),
-              Safehome(),
-              // custom carouel ,,,,, // whenever this slide change is called for it will get a random
-            ],
+                Emergency(),
+                Center(
+                  child: ShaderMask(
+                    shaderCallback:
+                        (bounds) => LinearGradient(
+                          colors: [Colors.red, Colors.orange],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ).createShader(bounds),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Explore Livesafe",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Livesafe(),
+                Safehome(),
+              ],
+            ),
           ),
         ),
       ),
