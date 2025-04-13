1750e7ab885cee49cdf656f7bcb2232b0de89d84
ashish <144021866+ashishexee@users.noreply.github.com>
2025-04-13 19:15:30 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,8 @@
+import com.android.build.gradle.BaseExtension
+import org.gradle.api.Project
+import org.gradle.api.tasks.Delete
+import org.gradle.api.file.Directory
+
 allprojects {
     repositories {
         google()
@@ -5,6 +10,18 @@ allprojects {
     }
 }
 
+subprojects {
+    project.afterEvaluate {
+        val androidExtension = project.extensions.findByName("android") as? BaseExtension
+        androidExtension?.let {
+            if (it.namespace == null) {
+                it.namespace = project.group.toString()
+            }
+        }
+    }
+}
+
+// Set custom build directory
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
@@ -12,10 +29,12 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Clean task
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
