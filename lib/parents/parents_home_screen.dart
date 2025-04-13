39c8a72677735469231c794188dfd47e7797f115
ashish <144021866+ashishexee@users.noreply.github.com>
2025-04-13 19:15:09 +0530

    parents home screen

---
+++ lib/parents/parents_home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ParentsHomeScreen extends StatelessWidget {
+  const ParentsHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
