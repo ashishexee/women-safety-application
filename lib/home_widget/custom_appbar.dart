cea0f743dc1f4bcb33205f238c3bce51a8b93df7
ashish <144021866+ashishexee@users.noreply.github.com>
2025-04-18 22:53:06 +0530

    ui improvements

--- lib/home_widget/custom_appbar.dart
+++ lib/home_widget/custom_appbar.dart
@@ -7,7 +7,7 @@ class CustomAppbar extends StatelessWidget {
   // List of quotes
   final List<String> quotes = [
     "Stay strong, stay safe.",
-    "Empowerment begins with safety.",
+    "Powerful and Protected.",
     "Your safety is your priority.",
     "Be aware, stay prepared.",
     "Courage is your shield.",
@@ -18,34 +18,28 @@ class CustomAppbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: ontap, // Trigger the callback when tapped
-      child: Container(
-        padding: const EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.pinkAccent, Colors.orangeAccent],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-          borderRadius: BorderRadius.circular(12.0),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1), // Reduced opacity
-              blurRadius: 8.0,
-              offset: const Offset(0, 4),
+      onTap: ontap,
+      child: Center(
+        child: ShaderMask(
+          shaderCallback:
+              (bounds) => const LinearGradient(
+                colors: [Colors.red, Colors.orange],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ).createShader(bounds),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              quotes[index],
+              style: const TextStyle(
+                fontSize: 23,
+                fontWeight: FontWeight.bold,
+                color: Colors.white, // This will be masked by the shader
+                letterSpacing: 1.2,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ],
-        ),
-        alignment: Alignment.center, // Center the content
-        child: Text(
-          quotes[index], // Display the quote based on the index
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 20.0,
-            fontWeight: FontWeight.w600,
-            letterSpacing: 1.2,
           ),
-          textAlign: TextAlign.center,
         ),
       ),
     );
